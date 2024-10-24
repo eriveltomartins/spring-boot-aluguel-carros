bf39731bd99a5b4208581a151e5b877930c11c25
eriveltomartins <134235926+eriveltomartins@users.noreply.github.com>
2024-10-24 08:43:58 -0300

    configuration

---
+++ AluguelCarros/src/test/java/erivelto/system/AluguelCarros/AluguelCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package erivelto.system.AluguelCarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluguelCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
